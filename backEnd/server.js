7c5ce2e3110f3f54fb5af4b86c79310f288d5f10
Gourav-Rajpurohit <mbc.raj.gaurav1986@gmail.com>
2026-02-17 19:45:19 +0530

    Refactor server.js to simplify DB connection
    Removed redundant database and cloudinary connection logic.

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,33 +1,20 @@
 import 'dotenv/config'
 import compression from 'compression'
 import './config/passport.js'
+import './config/cloudinary.js'
 
 import express from 'express'
 import cors from 'cors'
 import connectDB from './config/mongodb.js'
-import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoutes.js'
 import productRouter from './routes/productsRoutes.js'
 import cartRouter from './routes/cartRoutes.js'
 import orderRouter from './routes/orderRoutes.js'
-import passport from 'passport'
 
 const app = express()
 
-let isConnected = false;
-
-const init = async () => {
-  if (!isConnected) {
-    await connectDB();
-    await connectCloudinary();
-    isConnected = true;
-  }
-};
-
-app.use(async (req, res, next) => {
-  await init();
-  next();
-});
+// connect DB only once per instance
+await connectDB();
 
 app.use(express.json())
 app.use(cors())

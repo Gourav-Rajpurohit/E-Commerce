58f3c9e757e69698aca303a7195e49209d8c8f33
Gourav-Rajpurohit <mbc.raj.gaurav1986@gmail.com>
2026-02-17 19:03:54 +0530

    Refactor server initialization and connection handling
    Refactor server initialization to ensure database and cloudinary connections are established before handling requests.

--- backEnd/server.js
+++ backEnd/server.js
@@ -10,29 +10,36 @@ import userRouter from './routes/userRoutes.js'
 import productRouter from './routes/productsRoutes.js'
 import cartRouter from './routes/cartRoutes.js'
 import orderRouter from './routes/orderRoutes.js'
-
-import './config/passport.js'
 import passport from 'passport'
 
 const app = express()
-const port = process.env.PORT || 4000
-// const port = 4000
-connectDB()
-connectCloudinary()
+
+let isConnected = false;
+
+const init = async () => {
+  if (!isConnected) {
+    await connectDB();
+    await connectCloudinary();
+    isConnected = true;
+  }
+};
+
+app.use(async (req, res, next) => {
+  await init();
+  next();
+});
 
 app.use(express.json())
 app.use(cors())
 app.use(compression())
 
-//API Endpoints
-
 app.use('/api/user', userRouter);
 app.use('/api/product', productRouter)
 app.use('/api/cart', cartRouter)
 app.use('/api/orders', orderRouter)
-// console.log("STRIPE KEY:", process.env.STRIPE_SECRET_KEY)
+
 app.get('/', (req, res) => {
     res.send("API working")
 })
 
-app.listen(port, () => console.log(`server started over port ${port}`)) 
+export default app;
